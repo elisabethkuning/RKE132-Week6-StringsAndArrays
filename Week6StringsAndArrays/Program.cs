752932f097e8e254bc005bed6a6c0d43c72775eb
elisabethkuning <ekuning@tktk.ee>
2023-02-23 11:33:05 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,47 @@
+﻿string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+//string trimmedString = hello.Trim();
+//{
+//    Console.WriteLine($"{i} symbol in string; {hello[i]}");
+//}
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+//hello = hello.Replace(hello[0], 'H');
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 1;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    { 
+//        wordCounter++; 
+//    }  
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
+
+
+//for(int i = 0; i < hello.Length;i++)
+//{
+//    if(hello[i] == ' ')
+//    { 
+//        wordCounter++;
+//    } 
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter + 1} words.");
+
+//Console.WriteLine(hello.Length);
+
+//Console.WriteLine(trimmedString.Length);
+// viimase sümboli asukoht on alati (length-1)
